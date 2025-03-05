66a6ca958e600c4ab8cfaf09e224856c7ff05fd0
bilgates27 <bilgates270703@gmail.com>
2025-03-05 13:03:48 +0530

    refactor: update package.json, streamline App component, and add index.js for FloatingIcons export

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import FloatingIcons from './FloatingIcons'
 
 function App() {
   return (
-    <>
-      <FloatingIcons>
-      </FloatingIcons>
-    </>
+    <FloatingIcons>
+    </FloatingIcons>
   )
 }
 
